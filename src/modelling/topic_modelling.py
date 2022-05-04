fe6d7eab0401c168a85290f69e7bc348b7f43abd
Nicholas Vachon <niv@kapacity.dk>
2022-05-04 11:52:41 +0200

    Insure always 3 topics

--- src/modelling/topic_modelling.py
+++ src/modelling/topic_modelling.py
@@ -1,4 +1,5 @@
 import hydra
+import logging 
 
 import pandas as pd
 
@@ -6,7 +7,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from bertopic import BERTopic
 
 
-
+logger = logging.getLogger(__name__)
 
 def getTopicName(df_topic_info, topic_id):
 
@@ -19,7 +20,7 @@ def main(config):
     # Instantiate topic model
     vectorizer_model = CountVectorizer(ngram_range=(1, 2), stop_words="english")
     topic_model = BERTopic(language="english", 
-        min_topic_size=3, 
+        min_topic_size=3,                # higher -> less topics, default=10.
         embedding_model="all-MiniLM-L6-v2", 
         vectorizer_model=vectorizer_model
     )
@@ -27,8 +28,12 @@ def main(config):
     # Get data
     df_conv = pd.read_csv(config['first_message_path'])
 
-    # Train model and get topics id
-    df_conv['topics_id'], _ = topic_model.fit_transform(df_conv['first_msg_user'])
+    # Train model and get topics id until 3 topics found
+    n_topics = 0
+    while n_topics != 3:
+        df_conv['topics_id'], _ = topic_model.fit_transform(df_conv['first_msg_user'])
+        n_topics = df_conv['topics_id'].nunique()
+        logger.info(f'Number of topics found: {n_topics}')
 
     # Get topics info
     df_topic_info = topic_model.get_topic_info()
