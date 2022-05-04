c1b7f03cbdd937893e62bdd806280c1243b66bbb
Nicholas Vachon <niv@kapacity.dk>
2022-05-04 08:14:45 +0200

    Adeed pytest

--- test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-
-pd.testing.assert_frame_equal(pd.read_csv('/workspaces/ChatbotWiz/data/processed/topic_modelling/first_message_topic.csv'), pd.read_csv('/workspaces/ChatbotWiz/data/processed/topics.csv'))
-
